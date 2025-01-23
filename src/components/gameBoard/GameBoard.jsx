dab52516a25186d51607f62334d711976f40144a
Naya Fytali <125997387+nayafyt@users.noreply.github.com>
2025-01-23 16:46:51 +0200

    Initial commit

---
+++ src/components/gameBoard/GameBoard.jsx
@@ -0,0 +1,37 @@
+export default function GameBoard({ onSelectSquare, board }) {
+  //For the comments we needed the activePlayerSymbol as a prop
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
+  //   onSelectSquare();
+  // }
+  //Computed value of derived state (the state is from the app component the gameTurns)!!
+
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
